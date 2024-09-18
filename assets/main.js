bb65ff4acfe6c58057e540766803ee21014c0d89
zeru-program <justinebogor0609@gmail.com>
2024-09-18 20:27:02 +0700

    update controller page by justin

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+function openAlert() {
+  document.getElementById("popup-alert").style.display = "flex"
+}
+function closeAlert() {
+  document.getElementById("popup-alert").style.display = "none"
+}
